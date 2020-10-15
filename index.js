1f9c260bf021a53fcd8b953ee7aa38490e826872
zouhair <ettarak.zouhair@gmail.com>
2020-10-15 11:09:46 +0100

    Create Data Structure Array + Call into Routting

--- index.js
+++ index.js
@@ -2,23 +2,29 @@ const express = require('express');
 const app = express();
 
 // Route Default in Port http://localhost:3000/
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
     res.send('Hello Zouhair');
 });
-
-app.get('/Api/Product', (req,res) => {
-    res.send(['PC', 'Sourie', 'Cable']);
+// List Products
+app.get('/Api/Product', (req, res) => {
+    res.send(products);
 });
- 
-app.get('/Api/Product/:id', (req,res) => {
-    res.send(req.params.id);
+// Get  Product By id 
+app.get('/Api/Product/:id', (req, res) => {
+    //res.send(req.params.id);
     //res.send(req.params);
-});
-app.get('/Api/Product/:FirstName/:id', (req,res) => {
-    res.send(req.params);
+    const findProductById = products.find(element => element.productId == req.params.id);
+    res.send(findProductById);
+
 });
 // Change Port in CMD  => export port=4000 or others 
 const port = process.env.port || 3000;
-app.listen(port, ()=> console.log(`App working in Port :  http://localhost:/${port}`));
+app.listen(port, () => console.log(`App working in Port :  http://localhost:/${port}`));
 
 
+// Data Structures = Type  Array
+const products = [
+    { productId: 1, ProductName: 'PC DEL 2012' },
+    { productId: 2, ProductName: 'Lenovo' },
+    { productId: 3, ProductName: 'Mac' },
+];
