6b55a8155de379920332dd20969ac56ff4417008
zouhair <ettarak.zouhair@gmail.com>
2020-10-15 10:59:44 +0100

    Routting Params

--- index.js
+++ index.js
@@ -9,7 +9,14 @@ app.get('/', (req,res) => {
 app.get('/Api/Product', (req,res) => {
     res.send(['PC', 'Sourie', 'Cable']);
 });
-
+ 
+app.get('/Api/Product/:id', (req,res) => {
+    res.send(req.params.id);
+    //res.send(req.params);
+});
+app.get('/Api/Product/:FirstName/:id', (req,res) => {
+    res.send(req.params);
+});
 // Change Port in CMD  => export port=4000 or others 
 const port = process.env.port || 3000;
 app.listen(port, ()=> console.log(`App working in Port :  http://localhost:/${port}`));
